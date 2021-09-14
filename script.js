4f9b9b75aab0e32612bd7be434ecccbe09b9c996
TS93 <tschram93@yahoo.com>
2021-09-14 01:48:21 -0400

    added character length and a function for special characters

--- script.js
+++ script.js
@@ -13,80 +13,27 @@ var lowercase = document.querySelector('.lower');
 var special = document.querySelector('.spec');
 var number = document.querySelector('.number');
 var password = document.querySelector("#password");
-var btn = document.querySelector("#generate");
+// var btn = document.querySelector("#generate");
 // console.log(passwordEl)
 // I am using the character code numbers listed from https://www.w3schools.com/charsets/ref_html_ascii.asp
-const characters = {
-  uppercase: String.fromCharCode(Math.floor(Math.random() * 26) + 65),
-  lowercase: String.fromCharCode(Math.floor(Math.random() * 26) + 97),
-  number: String.fromCharCode(Math.floor(Math.random() *
-    10) + 48),
-  special: '!#$%&()*+,-./:;><=?@[]^_`~|{}'
+
+function uppercaseRandom() {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
 }
+// console.log(uppercaseRandom());
 
-//how many characters
-
-// generateBtn.addEventListener('change', () => {
-
-//   if (uppercaseEl.change) {} else {}
-//   if (specialEl.change) {} else {}
-//   if (numberEl.change) {} else {}
-//   // console.log(hasLower, hasUpper, hasNumber, hasSpecial);
-//   passwordEl.textContent = generatePassword(
-//     hasLower,
-//     hasUpper,
-//     hasNumber,
-//     hasSpecial);
-
-// });
-// Write password to the #password input
-function writePassword() {
-  var passwordSelection = "";
-
-  // var length = prompt("How many characters would you like your password to be? Pick between 6-12.")
+function lowercaseRandom() {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
 }
-// console.log(writePassword)
+// console.log(lowercaseRandom());
 
-function check(uppercaseEl) {
-  document.uppercaseEl.checked = true;
+function numberRandom() {
+  return String.fromCharCode(Math.floor(Math.random() *
+    10) + 48);
 }
-
-function uncheck(uppercaseEl) {
-  document.uppercaseEl.checked = false;
+// console.log(numberRandom());
+function specialRandom() {
+  const special = '!#$%&()*+,-./:;><=?@[]^_`~|{}';
+  return special[Math.floor(Math.random() * special.length)]
 }
-
-// lowercase
-function check(lowercaseEl) {
-  document.lowercaseEl.checked = true;
-}
-
-function uncheck(lowercaseEl) {
-  document.lowercaseEl.checked = false;
-}
-// special
-function check(specialEl) {
-  document.specialEl.checked = true;
-}
-
-function uncheck(specialEl) {
-  document.specialEl.checked = false;
-}
-// number
-function numcheck(numberEl) {
-  document.numberEl.checked = true;
-}
-
-function numuncheck(numberEl) {
-  document.numberEl.checked = false;
-}
-console.log(numcheck());
-console.log(numuncheck());
-
-for (let i = 0; i < length; i++) {
-  passwordFinal += passwordSelection[Math.floor(Math.random() * passwordSelection.length)]
-}
-
-var passwordFinal = ""
-document.querySelector("#password").innerText = passwordFinal;
-// }
-// console.log(writePassword);
+console.log(specialRandom());
